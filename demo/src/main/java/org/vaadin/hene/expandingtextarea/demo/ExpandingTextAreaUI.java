0445fcf36d97305f63c3892f928dc68c5599eed5
Markus Heikkinen <markush@softco.com>
2018-06-17 18:55:45 +0300

    Expanding TextArea Vaadin8 compatibility

--- demo/src/main/java/org/vaadin/hene/expandingtextarea/demo/ExpandingTextAreaUI.java
+++ demo/src/main/java/org/vaadin/hene/expandingtextarea/demo/ExpandingTextAreaUI.java
@@ -1,16 +1,21 @@
 package org.vaadin.hene.expandingtextarea.demo;
 
-import com.vaadin.annotations.PreserveOnRefresh;
-import com.vaadin.annotations.Theme;
-import com.vaadin.v7.data.Property;
-import com.vaadin.ui.*;
-import com.vaadin.ui.themes.ValoTheme;
 import org.vaadin.hene.expandingtextarea.ExpandingTextArea;
 import org.vaadin.hene.expandingtextarea.ExpandingTextArea.RowsChangeEvent;
 import org.vaadin.hene.expandingtextarea.ExpandingTextArea.RowsChangeListener;
 
+import com.vaadin.annotations.PreserveOnRefresh;
+import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.v7.data.Property;
+import com.vaadin.v7.data.Property.ValueChangeEvent;
+import com.vaadin.v7.ui.CheckBox;
+import com.vaadin.v7.ui.NativeSelect;
+import com.vaadin.v7.ui.VerticalLayout;
 
 @Title("ExpandingTextArea")
 @Theme("valo")
@@ -29,13 +34,19 @@ public class ExpandingTextAreaUI extends UI {
 		Label title = new Label("ExpandingTextArea");
 		title.addStyleName(ValoTheme.LABEL_HUGE);
 		content.addComponent(title);
-		
-		content.addComponent(new Label(
-				"Write text to the field and see how its height expands."));
+
+		content.addComponent(new Label("Write text to the field and see how its height expands."));
 		final ExpandingTextArea expandingTextArea = new ExpandingTextArea();
 		expandingTextArea.setImmediate(true);
 		expandingTextArea.setWidth("300px");
-		//expandingTextArea.setValue("Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed imperdiet magna. Sed cursus nunc ac dui hendrerit sed auctor diam pellentesque. Etiam hendrerit lobortis lectus vitae interdum. Nam non elementum sem. Aenean feugiat metus nec libero ullamcorper pharetra. Suspendisse varius dolor quis magna egestas id mollis nisi ullamcorper. Fusce a interdum neque. Pellentesque et nisi nisi. In hendrerit nisl ut odio mattis ut pulvinar ligula porta.");
+		// expandingTextArea.setValue("Lorem ipsum dolor sit amet, consectetur
+		// adipiscing elit. In sed imperdiet magna. Sed cursus nunc ac dui
+		// hendrerit sed auctor diam pellentesque. Etiam hendrerit lobortis
+		// lectus vitae interdum. Nam non elementum sem. Aenean feugiat metus
+		// nec libero ullamcorper pharetra. Suspendisse varius dolor quis magna
+		// egestas id mollis nisi ullamcorper. Fusce a interdum neque.
+		// Pellentesque et nisi nisi. In hendrerit nisl ut odio mattis ut
+		// pulvinar ligula porta.");
 		content.addComponent(expandingTextArea);
 
 		final Label rowsLabel = new Label("" + expandingTextArea.getRows());
@@ -49,17 +60,17 @@ public class ExpandingTextAreaUI extends UI {
 
 		final CheckBox appendExtraRow = new CheckBox("Append extra row", expandingTextArea.isAppendExtraRow());
 		appendExtraRow.addValueChangeListener(new Property.ValueChangeListener() {
-			@Override
-			public void valueChange(Property.ValueChangeEvent event) {
+
+			public void valueChange(ValueChangeEvent event) {
 				expandingTextArea.setAppendExtraRow(appendExtraRow.getValue());
 			}
+
 		});
 		content.addComponent(appendExtraRow);
 
 		final NativeSelect maxRows = new NativeSelect("Max rows");
 		maxRows.addValueChangeListener(new Property.ValueChangeListener() {
-			@Override
-			public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+			public void valueChange(ValueChangeEvent valueChangeEvent) {
 				expandingTextArea.setMaxRows((Integer) maxRows.getValue());
 			}
 		});
